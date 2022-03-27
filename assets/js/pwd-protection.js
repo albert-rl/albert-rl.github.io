e91d4cac8fbb9ed0f64e245233e0ec60e3a2b758
albert-rl <75096864+albert-rl@users.noreply.github.com>
2022-03-27 19:01:44 +0200

    Add files via upload
    New site version

---
+++ assets/js/pwd-protection.js
@@ -0,0 +1,39 @@
+"use strict"
+
+var button = document.querySelectorAll('[data-id="submit"]')
+var password = document.querySelectorAll('[data-id="pwd"]')
+
+function login(secret) {
+  var hash = sha1(secret)
+  var url = hash + "/index.html"
+
+  var request = new XMLHttpRequest()
+  request.open('GET', url, true)
+
+  request.onload = function() {
+    if (request.status >= 200 && request.status < 400) {
+      window.location = url
+    } else {
+      parent.location.hash = hash
+      password[0].setAttribute('placeholder', 'Contraseña incorrecta, prueba otra vez')
+      password[0].value = ''
+    }
+  }
+  request.onerror = function() {
+    parent.location.hash = hash
+    password[0].setAttribute('placeholder', 'Contraseña incorrecta, prueba otra vez')
+    password[0].value = ''
+  }
+  request.send()
+}
+
+button[0].addEventListener("click", function() {
+  login(password[0].value)
+})
+
+document.onkeydown = function(e) {
+  e = e || window.event
+  if (e.keyCode == 13) {
+    login(password[0].value)
+  }
+}
